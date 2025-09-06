1fb77ff662abdd983dce9824b9610b6a6233c41b
WORLDREPS <businessreps1832@gmail.com>
2025-09-06 18:56:28 +0200

    Add files via upload

---
+++ categories.js
@@ -0,0 +1,14 @@
+
+const categories = [
+  {name:"Camisetas", subcategories:["Camisetas cortas","Camisetas de manga larga","Polos","Camisetas de tirantes"]},
+  {name:"Pantalones", subcategories:["Pantalones cortos","Pantalones largos","Pantalones cargo","Leggins","Pantalones capri","Bañadores"]},
+  {name:"Abrigos / Chaquetas", subcategories:["Plumas","Chalecos","Cortavientos"]},
+  {name:"Sudaderas", subcategories:[]},
+  {name:"Zapatillas", subcategories:[]},
+  {name:"Accesorios", subcategories:["Collares","Ropa interior","Anillos","Pendientes","Pulseras","Gafas de sol","Cinturones","Gorros / Gorras","Carteras","Riñoneras"]},
+  {name:"Bolsos", subcategories:["Bolsos de mujer","Bolsos de hombre","Mochilas","Otros bolsos"]},
+  {name:"Relojes", subcategories:["Gama media","Gama alta"]},
+  {name:"Electrónica", subcategories:["Relojes digitales","Auriculares","Móviles","Altavoces","Accesorios electrónicos"]},
+  {name:"Perfumes", subcategories:[]},
+  {name:"Toys", subcategories:[]}
+];
